48dd55c00cd12ec87ce92b69a3919043fb183395
Juan C <juanconrod@protonmail.com>
2024-07-23 13:33:04 +0200

    Typo: `load-state` instead of `load-state-diff`

--- 4-external-computation/src/ExternalComputation.sol
+++ 4-external-computation/src/ExternalComputation.sol
@@ -6,7 +6,7 @@ import {Counter} from "./Counter.sol";
 import {SaveAddress} from "./SaveAddress.sol";
 
 // forge script src/ExternalComputation.sol:ExternalComputation --sig severalCountersDump --ffi
-// kontrol load-state-diff RecordedState state-dump/StateDump.json --contract-names state-dump/AddressNames.json --output-dir test
+// kontrol load-state RecordedState state-dump/StateDump.json --contract-names state-dump/AddressNames.json --output-dir test
 // kontrol prove --mt prove_multiple_counters --init-node-from-dump state-dump/StateDump.json
 
 contract ExternalComputation is SaveAddress, Test {
